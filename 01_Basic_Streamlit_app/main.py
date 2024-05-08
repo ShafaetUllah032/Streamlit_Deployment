d2f975f1800c7108a3ee70cfddaab37ae5aae91c
Safaet-ullah <shafetullah02@gmail.com>
2024-05-08 13:52:11 +0600

    ML model deployment journey begin

---
+++ 01_Basic_Streamlit_app/main.py
@@ -0,0 +1,4 @@
+import streamlit as st
+import pandas as pd
+st.write(pd.DataFrame({'titu':[1,2,3],
+             'chandni':[4,5,6]}))
---
+++ 01_Basic_Streamlit_app/text_element.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+
+# give you a app title
+st.title("Text Element Streamlit")
+
+# Header
+st.header("Basic app fundamentals")
+
+# Sub header 
+st.subheader("working to handle text")
+
+# markdown
+st.markdown('''
+...     :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
+...     :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
+
+# Caption
+st.caption('A caption with _italics_ :blue[colors] and emojis :sunglasses:')
+
+# Code block
+
+st.code( """ import numpy as np \n np.asarray(img) \n plt.imshow(img)""" )
+
+# preformated text
+
+st.text("Raw text")
+
+# Latex
+st.latex("2^2 = 4")
+
+# Divider
+
+st.text("upper part")
+st.divider()
+st.text("lower part")
+
+# st.wrtie
+
+st.write("Some raws text , with write options")
+
