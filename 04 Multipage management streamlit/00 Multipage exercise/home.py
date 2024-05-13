9f50b9c2ebcf5993807cde64e58d174f17738b19
Safaet-ullah <shafetullah02@gmail.com>
2024-05-13 10:19:09 +0600

    multipage management exercise added

---
+++ 04 Multipage management streamlit/00 Multipage exercise/home.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="Homepage",
+    page_icon="🏡",
+    layout="centered",
+    initial_sidebar_state="expanded"
+)
+
+if all(key not in st.session_state.keys() for key in ('product','x1','x2')):
+    st.session_state['x1']=0
+    st.session_state['x2']=0
+    st.session_state['product']=0
+
+# Workaround from: https://stackoverflow.com/questions/74968179/session-state-is-reset-in-streamlit-multipage-app
+
+if "product" not in st.session_state:
+    st.session_state["product"]=0
+
+def do_product(x1,x2):
+    st.session_state["product"]=x1*x2
+
+def keep(key):
+    st.session_state[key]=st.session_state[f"_{key}"]
+
+def unkeep(key):
+    st.session_state[f"_{key}"]=st.session_state[key]
+
+
+if __name__=="__main__":
+    st.title("Homepage") 
+    col1,col2=st.columns(2)
+
+    with col1:
+        unkeep('x1')
+        x1=st.number_input("pick a number", 0 ,10,key="_x1",on_change=keep,args=(('x1',)))
+    
+    with col2:
+        unkeep('x2')
+        x2=st.number_input("pick a number",0,10,key="_x2",on_change=keep,args=(('x2',)))
+
+    st.button("Product",type="primary",on_click=do_product,args=((x1,x2)))
+
+    st.write(st.session_state)
